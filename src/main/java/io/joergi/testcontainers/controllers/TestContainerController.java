3c93502f5fb7f3d361e270b133b8fe7d192ecffb
joergi <1439809+joergi@users.noreply.github.com>
2020-07-10 10:30:57 +0200

    first commit

---
+++ src/main/java/io/joergi/testcontainers/controllers/TestContainerController.java
@@ -0,0 +1,16 @@
+package io.joergi.testcontainers.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class TestContainerController {
+
+
+    @GetMapping(value = "/")
+    void test(){
+        log.info("in");
+    }
+}
