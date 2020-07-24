0c4a3dc7dc551db41ba788695829008135af728c
joergi <1439809+joergi@users.noreply.github.com>
2020-07-24 13:49:44 +0200

    little refactoring #1

--- src/main/java/io/joergi/testcontainers/services/UploadServiceGcp.java
+++ src/main/java/io/joergi/testcontainers/services/UploadServiceGcp.java
@@ -26,6 +26,10 @@ public class UploadServiceGcp {
     private Credentials credentials;
     
     private Storage storage;
+    
+    private static final String GCP_BUCKET = "testbucket_joergi";
+
+    private static final String GCP_PROJCT_ID = "inbound-domain-284307";
 
     public UploadServiceGcp(GCPConfig gcpConfig) throws FileNotFoundException, IOException {
         this.credentials = gcpConfig.createCredentials();
@@ -37,33 +41,23 @@ public class UploadServiceGcp {
                 .getService();
     }
     
-    private static final String GCP_BUCKET = "testbucket_joergi";
-
-    private static final String GCP_PROJCT_ID = "inbound-domain-284307";
-    
-    
     public void uploadFile(File file) throws IOException {
 
         String filename = "test" + new Random().nextLong() + ".jpg";
 
-        // The path to your file to upload
-        String filePath = "/home/joergi/dev/projects/joergi/tryout_testcontainers/src/main/resources/github.jpg";
+        String filePath = file.getAbsolutePath();
         
-       
-        
-        
-        BlobId blobId = BlobId.of(GCP_BUCKET, filename);
-        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
+        BlobInfo blobInfo = BlobInfo.newBuilder(BlobId.of(GCP_BUCKET, filename)).build();
         
         Blob blob = storage.create(blobInfo, Files.readAllBytes(Paths.get(filePath)));
-        delete(blob.getBlobId(), filename);
+        
         log.info("file: {}  with filename {} with blob {} has been uploaded", filePath, filename, blob.toString() ); 
-
+        
+        delete(blob.getBlobId(), filename);
     }
     
     private void delete(BlobId blobid, String filename) {
         log.info("filename {}  was deletedt {}", filename, storage.delete(blobid));
-        
 
     }
 }
